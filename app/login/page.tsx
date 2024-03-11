67b2fe1898bfce31822c5de8fa70751c17d33c97
Yargo Valério <yargovale@hotmail.com>
2024-03-11 13:58:39 -0300

    [PDG-5] planejando tela de login

--- app/login/page.tsx
+++ app/login/page.tsx
@@ -3,12 +3,15 @@ import { headers } from "next/headers";
 import { createClient } from "@/utils/supabase/server";
 import { redirect } from "next/navigation";
 import { SubmitButton } from "./submit-button";
+import { useState } from "react";
 
 export default function Login({
   searchParams,
 }: {
   searchParams: { message: string };
 }) {
+
+
   const signIn = async (formData: FormData) => {
     "use server";
 
@@ -52,68 +55,112 @@ export default function Login({
   };
 
   return (
-    <div className="flex-1 flex flex-col w-full px-8 sm:max-w-md justify-center gap-2">
-      <Link
-        href="/"
-        className="absolute left-8 top-8 py-2 px-4 rounded-md no-underline text-foreground bg-btn-background hover:bg-btn-background-hover flex items-center group text-sm"
-      >
-        <svg
-          xmlns="http://www.w3.org/2000/svg"
-          width="24"
-          height="24"
-          viewBox="0 0 24 24"
-          fill="none"
-          stroke="currentColor"
-          strokeWidth="2"
-          strokeLinecap="round"
-          strokeLinejoin="round"
-          className="mr-2 h-4 w-4 transition-transform group-hover:-translate-x-1"
-        >
-          <polyline points="15 18 9 12 15 6" />
-        </svg>{" "}
-        Back
-      </Link>
+    <div className="mx-auto min-h-screen overflow-hidden">
+      <section id='login' className='h-screen'>
+        {/* <div className="flex-1 flex flex-col w-full px-8 sm:max-w-md justify-center gap-2"> */}
+        <div className="bg-neutral-900/[.80] min-h-screen  flex justify-center items-center flex-col">
+          <div className="bg-neutral-900/[.90] p-4 w-fit rounded-lg">
+            <p className='font-bold text-[5rem] text-gray-50 text-center'>
+              PDG
+            </p>
 
-      <form className="animate-in flex-1 flex flex-col w-full justify-center gap-2 text-foreground">
-        <label className="text-md" htmlFor="email">
-          Email
-        </label>
-        <input
-          className="rounded-md px-4 py-2 bg-inherit border mb-6"
-          name="email"
-          placeholder="you@example.com"
-          required
-        />
-        <label className="text-md" htmlFor="password">
-          Password
-        </label>
-        <input
-          className="rounded-md px-4 py-2 bg-inherit border mb-6"
-          type="password"
-          name="password"
-          placeholder="••••••••"
-          required
-        />
-        <SubmitButton
-          formAction={signIn}
-          className="bg-green-700 rounded-md px-4 py-2 text-foreground mb-2"
-          pendingText="Signing In..."
-        >
-          Sign In
-        </SubmitButton>
-        <SubmitButton
-          formAction={signUp}
-          className="border border-foreground/20 rounded-md px-4 py-2 text-foreground mb-2"
-          pendingText="Signing Up..."
-        >
-          Sign Up
-        </SubmitButton>
-        {searchParams?.message && (
-          <p className="mt-4 p-4 bg-foreground/10 text-foreground text-center">
-            {searchParams.message}
-          </p>
-        )}
-      </form>
-    </div>
+            <form className="animate-in flex-1 flex flex-col w-full justify-center gap-2 text-foreground">
+              {/* <label className="text-md" htmlFor="email">
+              Email
+            </label> */}
+              <input
+                className="rounded-md px-4 py-2 bg-inherit border mb-6"
+                name="email"
+                placeholder="Email"
+                required
+              />
+              {/* <label className="text-md" htmlFor="password">
+              Password
+            </label> */}
+              <input
+                className="rounded-md px-4 py-2 bg-inherit border mb-6"
+                type="password"
+                name="password"
+                placeholder="Senha"
+                required
+              />
+              <SubmitButton
+                formAction={signIn}
+                className="bg-green-700 rounded-md px-4 py-2 text-foreground mb-2"
+                pendingText="Signing In..."
+              >
+                Entrar
+              </SubmitButton>
+              <Link
+                href='#forgot'
+                className="text-slate-50 hover:underline box-content mx-auto"
+              >
+                Esqueci minha senha
+              </Link>
+
+              <hr className="bg-slate-50" />
+
+              <p className='font-bold text-[1rem] text-gray-50 text-center'>
+                Não possui conta?
+              </p>
+
+              <SubmitButton
+                formAction={signUp}
+                className="border border-foreground/20 rounded-md px-4 py-2 text-foreground mb-2"
+                pendingText="Signing Up..."
+              >
+                Cadastre-se
+              </SubmitButton>
+              {searchParams?.message && (
+                <p className="mt-4 p-4 bg-foreground/10 text-foreground text-center">
+                  {searchParams.message}
+                </p>
+              )}
+            </form>
+          </div>
+        </div>
+      </section>
+      <section id='forgot'>
+        <div className="bg-neutral-900/[.80] min-h-screen  flex justify-center items-center flex-col">
+          <div className="bg-neutral-900/[.90] p-4 w-fit rounded-lg">
+            <p className='font-bold text-[5rem] text-gray-50 text-center'>
+              PDG
+            </p>
+
+            <form className="animate-in flex-1 flex flex-col w-full justify-center gap-2 text-foreground">
+              {/* <label className="text-md" htmlFor="email">
+              Email
+            </label> */}
+              <input
+                className="rounded-md px-4 py-2 bg-inherit border mb-6"
+                name="email"
+                placeholder="Email"
+                required
+              />
+              <SubmitButton
+                formAction={signIn}
+                className="bg-green-700 rounded-md px-4 py-2 text-foreground mb-2"
+                pendingText="Signing In..."
+              >
+                Enviar
+              </SubmitButton>
+
+              <Link
+                href='#login'
+                className="text-slate-50 hover:underline box-content mx-auto"
+              >
+                Voltar
+              </Link>
+
+              {searchParams?.message && (
+                <p className="mt-4 p-4 bg-foreground/10 text-foreground text-center">
+                  {searchParams.message}
+                </p>
+              )}
+            </form>
+          </div>
+        </div>
+      </section>
+    </div >
   );
 }
