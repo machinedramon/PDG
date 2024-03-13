19374a3b33e56b9966b5abf7384036d56342592e
diauferr <diaugustdev@gmail.com>
2024-03-13 20:31:36 -0300

    🐛 fix(PDG-2): apply urgent change to content area implementation on /feed, added moneytag verification script

--- app/feed/page.tsx
+++ app/feed/page.tsx
@@ -2,24 +2,15 @@ import dynamic from "next/dynamic";
 import Nav from "../../components/Feed/Nav";
 import Content from "@/components/Feed/Content";
 
-import Head from "next/head";
-
 const Sidebar = dynamic(() => import("../../components/Feed/Sidebar"));
-import GoogleAdSense from "@/components/Feed/Ad";
 
 export default function Feed() {
   return (
     <div className="flex min-h-screen">
-      <Head>
-        {/* Adicionar a meta tag do Google AdSense */}
-        <meta name="google-adsense-account" content="ca-pub-8593648212468558" />
-      </Head>
       {/* Sidebar */}
       <div className="bg-secondaryBlack w-64 h-screen fixed border-r border-[#29292F]">
         <Sidebar />
       </div>
-
-      <GoogleAdSense />
 
       {/* Container para Header, Conteúdo Principal, Áreas 4 e 5 */}
       <div className="pl-64 flex flex-col w-full">
