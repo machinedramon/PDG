88ac6a5f09fea9eeed9235211e88ff5887586de9
diauferr <diaugustdev@gmail.com>
2024-03-13 17:34:14 -0300

    🐛 fix(PDG-2): apply urgent change to content area implementation on /feed, in progress

--- app/feed/page.tsx
+++ app/feed/page.tsx
@@ -2,12 +2,18 @@ import dynamic from "next/dynamic";
 import Nav from "../../components/Feed/Nav";
 import Content from "@/components/Feed/Content";
 
+import Head from "next/head";
+
 const Sidebar = dynamic(() => import("../../components/Feed/Sidebar"));
 import GoogleAdSense from "@/components/Feed/Ad";
 
 export default function Feed() {
   return (
     <div className="flex min-h-screen">
+      <Head>
+        {/* Adicionar a meta tag do Google AdSense */}
+        <meta name="google-adsense-account" content="ca-pub-8593648212468558" />
+      </Head>
       {/* Sidebar */}
       <div className="bg-secondaryBlack w-64 h-screen fixed border-r border-[#29292F]">
         <Sidebar />
