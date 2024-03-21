552eb8d078da3056998c63e40ed33323eafbeecc
Yargo Valerio dos Santos Abou Ali <yargo@cpqd.com.br>
2024-03-20 22:09:49 -0300

    :white_check_mark:(PDG-5) login/subscribe screen completed

--- app/(account)/login/page.tsx
+++ app/(account)/login/page.tsx
@@ -1,6 +1,7 @@
 'use client'
 
 import { SubmitButton } from "./submit-button";
+import styles from "./styles.module.css";
 
 import { signIn, signUp } from './sign-functions'
 import { useEffect, useState } from "react";
@@ -8,13 +9,18 @@ import { useEffect, useState } from "react";
 import Image from "next/image";
 
 import LogoAnimated from "@/assets/images/logo-animated-full.svg";
-import BgLogin from '@/assets/images/login_bg.png';
 
 export default function Login({
   searchParams,
 }: {
   searchParams: { message?: string, success?: string };
 }) {
+
+  const [userEmail, setUserEmail] = useState<string>('')
+  const [userResetEmail, setUserResetEmail] = useState<string>('')
+  const [userPassword, setUserPassword] = useState<string>('')
+
+  const animatedGradient = styles.combinedEffect;
 
   const [loginOption, setLoginOption] = useState<boolean>(true) //true = login; false =  forgot password
 
@@ -28,69 +34,81 @@ export default function Login({
 
   return (
     <div className="mx-auto  min-h-screen flex justify-center items-center bg-login-bg bg-cover bg-no-repeat">
-      <div className="bg-pdgBlack-900 m-4 p-4 max-w-[949px] w-[100%] rounded-lg gap-8 justify-center flex flex-row">
-        <Image src={LogoAnimated} alt="PDG" className="h-auto w-cover flex flex-1" />
-        <div className='flex flex-1 p-8 gap-8 flex-col'>
-          <div className='gap-4 flex flex-col'>
-            <h1 className='font-bold text-4xl text-pdgWhite-50 w-max'>Pronto para começar?</h1>
-            <p className='font-normal text-base text-pdgWhite-50 text-center'>Entre ou cadastre-se para começar a explorar!</p>
-          </div>
+      <div className="bg-pdgBlack-900 m-8 py-8 px-2 sm:px-4 md:px-8 gap-8 max-w-[949px] w-[80%] md:w-full rounded-lg  justify-center flex flex-col md:flex-row">
+        <div className='h-auto w-full flex flex-1 '>
+          <Image src={LogoAnimated} alt="PDG" className="w-fit" />
+        </div>
+        <div className='flex flex-1 px-4 py-0 md:p-8 gap-8 flex-col'>
           {loginOption ?
-            <form className="animate-in flex-1 flex flex-col w-full justify-center gap-4 text-foreground">
-              <div className="flex flex-col gap-2">
-                <label className="text-normal text-base text-pdgWhite-50" htmlFor="email">
-                  Email<span className='text-red-600'> *</span>
-                </label>
-
-                <input
-                  className={`bg-pdgBlack-500 border-pdgBlack-500 rounded-lg p-4 gap-[10px] bg-inherit border text-pdgWhite-50 focus:bg-slate-800 ${searchParams?.message && 'border-red-500'}`}
-                  name="email"
-                  type="email"
-                  required
-                />
+            <form className="animate-in flex-1 flex flex-col w-full justify-center gap-8 text-foreground">
+              <div className='gap-4 flex flex-col'>
+                <h1 className='font-bold text-[1.75rem] md:text-4xl text-pdgWhite-50 text-center'>
+                  Pronto para começar?
+                </h1>
+                <p className='font-normal text-base text-pdgWhite-50 text-center'>
+                  Entre ou cadastre-se para começar a explorar!
+                </p>
               </div>
-              <div className="flex flex-col gap-2">
-                <label className="text-normal text-base text-pdgWhite-50" htmlFor="password">
-                  Senha<span className='text-red-600'> *</span>
-                </label>
+              <div className="flex flex-col gap-4">
+                <div className="flex flex-col gap-2">
+                  <label className="text-normal text-base text-pdgWhite-50" htmlFor="email">
+                    Email<span className='text-red-600'> *</span>
+                  </label>
 
-                <input
-                  className={`bg-pdgBlack-500 border-pdgBlack-500 rounded-lg p-4 gap-[10px] bg-inherit border text-pdgWhite-50 focus:bg-slate-800 ${searchParams?.message && 'border-red-500'}`}
-                  name="password"
-                  type="password"
-                  required
-                />
+                  <input
+                    className={`bg-pdgBlack-500 border border-pdgBlack-500 rounded-lg p-4 gap-[10px] bg-inherit text-pdgWhite-50 focus:bg-slate-800 ${searchParams?.message && 'border-red-500'}`}
+                    name="email"
+                    type="email"
+                    value={userEmail}
+                    onChange={e => setUserEmail(e.target.value)}
+                    required
+                  />
+                </div>
+                <div className="flex flex-col gap-2">
+                  <label className="text-normal text-base text-pdgWhite-50" htmlFor="password">
+                    Senha<span className='text-red-600'> *</span>
+                  </label>
+
+                  <input
+                    className={`bg-pdgBlack-500 border-pdgBlack-500 rounded-lg p-4 gap-[10px] bg-inherit border text-pdgWhite-50 focus:bg-slate-800 ${searchParams?.message && 'border-red-500'}`}
+                    name="password"
+                    type="password"
+                    value={userPassword}
+                    onChange={e => setUserPassword(e.target.value)}
+                    required
+                  />
+                  <button
+                    onClick={toogleLoginOption} type="button"
+                    className="text-pdgBlue-500 hover:underline box-content text-left text-xs font-medium"
+                  >
+                    Esqueceu a senha?
+                  </button>
+                </div>
               </div>
-              <button
-                onClick={toogleLoginOption} type="button"
-                className="text-pdgBlue-500 hover:underline box-content text-left text-xs font-medium"
-              >
-                Esqueci minha senha
-              </button>
-
-              <div className='flex gap-4 items-center'>
-
+              <div className='flex gap-4 items-center flex-col sm:flex-row'>
                 <SubmitButton
                   formAction={signIn}
-                  className=" rounded-lg border border-pdgBlack-500 py-[18px] px-4 gap-[10px]
-                  text-base font-bold text-pdgWhite-50 flex flex-1 justify-center hover:scale-105
-                  bg-gradient-to-r from-[#FF00FF] to-[#00FFFF]
-                  active:border active:border-pdgWhite-50
-                  transition duration-300 ease-in-out"
+                  className={`w-full rounded-lg  py-[18px] px-4 gap-[10px]
+                  text-base font-bold text-pdgWhite-50 flex flex-1 justify-center 
+                  ${(userPassword && userEmail) === '' ? 'bg-pdgBlack-500 cursor-not-allowed' : animatedGradient}
+                  ${(userPassword && userEmail) !== '' && "hover:scale-105 active:border active:border-pdgWhite-50"}
+                  transition duration-300 ease-in-out`}
+                  disabled={(userPassword || userEmail) === '' ? true : false}
                   pendingText="Entrando..."
                 >
                   Entrar
                 </SubmitButton>
 
-                <p className='text-base font-bold text-[#fff]'>ou</p>
+                <p className='text-base font-bold text-[#fff] hidden sm:flex'>ou</p>
 
                 <SubmitButton
                   formAction={signUp}
-                  className="bg-pdgBlack-500 rounded-lg border border-pdgBlack-500 py-[18px] px-4 gap-[10px]
-                  text-base font-bold text-pdgWhite-50  flex flex-1 justify-center hover:scale-105
-                  bg-gradient-to-r from-[#FF00FF] to-[#00FFFF]
-                  active:border active:border-pdgWhite-50
-                  transition duration-300 ease-in-out"
+                  className={`w-full rounded-lg py-[18px] px-4 gap-[10px]
+                  text-base font-bold text-pdgWhite-50 flex flex-1 justify-center 
+                  ${(userPassword && userEmail) === '' ? 'bg-pdgBlack-500 cursor-not-allowed' : animatedGradient}
+                  ${(userPassword && userEmail) !== '' && "hover:scale-105 active:border active:border-pdgWhite-50"}
+                  transition duration-300 ease-in-out`}
+                  disabled={(userPassword || userEmail) === '' ? true : false}
                   pendingText="Registrando..."
                 >
                   Registrar
@@ -103,36 +121,64 @@ export default function Login({
               </div>
             </form>
             :
-            <form className="animate-in flex-1 flex flex-col w-full justify-center gap-4 text-foreground">
-              <label className="text-md text-slate-50 text-xl" htmlFor="email">
-                Recupere sua conta
-              </label>
-              <input
-                className="rounded-md px-4 py-2 bg-inherit border mb-6 placeholder:text-slate-50 text-slate-50 focus:bg-slate-800"
-                name="email"
-                placeholder="Email"
-                required
-              />
-              <SubmitButton
-                formAction={signIn}
-                className="bg-sky-400 rounded-md px-4 py-2 text-foreground mb-2 hover:bg-sky-600 active:bg-sky-500 transition-all text-slate-50 font-bold"
-                pendingText="Entrando..."
-              >
-                Enviar
-              </SubmitButton>
-
-              <button
-                onClick={toogleLoginOption}
-                className="text-slate-50 hover:underline box-content mx-auto"
-              >
-                Voltar
-              </button>
-              {searchParams?.message && (
-                <p className="mt-4 p-4 bg-foreground/10 text-foreground text-center">
-                  {searchParams.message}
+            <form className="animate-in flex-1 flex flex-col w-full justify-center gap-8 text-foreground">
+              <div className='gap-4 flex flex-col'>
+                <h1 className='font-bold text-4xl text-pdgWhite-50 text-center'>
+                  Esqueceu sua senha?
+                </h1>
+                <p className='font-normal text-base text-pdgWhite-50 text-center'>
+                  Calma que a gente te ajuda a recuperar
                 </p>
-              )}
+              </div>
+              <div className="flex flex-col gap-2">
+                <label className="text-normal text-base text-pdgWhite-50" htmlFor="email">
+                  Email<span className='text-red-600'> *</span>
+                </label>
+
+                <input
+                  className={`bg-pdgBlack-500 border-pdgBlack-500 rounded-lg p-4 gap-[10px] bg-inherit border text-pdgWhite-50 focus:bg-slate-800 ${searchParams?.message && 'border-red-500'}`}
+                  name="email"
+                  type="email"
+                  value={userResetEmail}
+                  onChange={e => setUserResetEmail(e.target.value)}
+                  required
+                />
+              </div>
+              <div className='flex gap-4 items-center'>
+
+                <SubmitButton
+                  onClick={toogleLoginOption}
+                  className=" rounded-lg border-lg border-pdgBlack-500 py-[18px] px-4 gap-[10px]
+                  text-base font-bold text-pdgWhite-50  flex flex-1 justify-center hover:scale-105
+                  bg-red-500
+                  active:border active:border-pdgWhite-50
+                  transition duration-300 ease-in-out"
+                  pendingText="Registrando..."
+                >
+                  Voltar
+                </SubmitButton>
+
+                <SubmitButton
+                  formAction={() => { }}
+                  className={`rounded-lg  py-[18px] px-4 gap-[10px]
+                  text-base font-bold text-pdgWhite-50 flex flex-1 justify-center 
+                  ${userResetEmail === '' ? 'bg-pdgBlack-500 cursor-not-allowed' : animatedGradient}
+                  ${userResetEmail !== '' && "hover:scale-105 active:border active:border-pdgWhite-50"}
+                  transition duration-300 ease-in-out`}
+                  disabled={userResetEmail === '' ? true : false}
+                  pendingText="Enviando..."
+                >
+                  Enviar
+                </SubmitButton>
+
+                {searchParams?.message && (
+                  <p className="mt-4 p-4 bg-foreground/10 text-foreground text-center text-red-500">
+                    Usuário ou senha inválidos
+                  </p>
+                )}
+              </div>
             </form>
+
           }
         </div>
       </div>
